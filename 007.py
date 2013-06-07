f5881555be85fe88a9b14eda2df95e6230328260
Charles Feng <charles@charles.io>
2013-06-06 23:30:34 -0700

    problem 7

---
+++ 007.py
@@ -0,0 +1,23 @@
+# project euler: problem 7 (http://projecteuler.net/problem=7)
+
+# (c) 2013 charles feng (https://github.com/charlesfeng)
+# shared under the mit license (http://www.opensource.org/licenses/mit)
+
+import math
+
+def primes(l):
+  a = [False, False] + [True] * (l - 2)
+
+  for (i, p) in enumerate(a):
+    if p:
+      yield i
+      a[i * i : l : i] = [False] * int(math.ceil(float(l - i * i) / i))
+
+ps = []
+for i in primes(120000):
+  ps.append(i)
+
+print ps[10001]
+
+# answer: 104759
+# runtime: 0.1s
