ff2e9d4140c3a09702833bd3c1c2d318ef1f2452
Charles Feng <charles@charles.io>
2013-06-07 01:09:28 -0700

    make 12 slightly faster by using a list of primes

--- 012.py
+++ 012.py
@@ -3,32 +3,44 @@
 # (c) 2013 charles feng (https://github.com/charlesfeng)
 # shared under the mit license (http://www.opensource.org/licenses/mit)
 
+import math
+
+def primes(l):
+  a = [False, False] + [True] * (l - 2)
+
+  for (i, p) in enumerate(a):
+    if p:
+      yield i
+      a[i * i : l : i] = [False] * int(math.ceil(float(l - i * i) / i))
+
 def prod(l):
   return reduce(lambda x, y: x * y, l, 1)
 
-def nd(n):
+def nd(n, ps):
   a = dict()
-  p = 2
+  i, p = 0, ps[0]
   
   while p < n:
     if n % p:
-      p += 1
+      i, p = i + 1, ps[i + 1]
     else:
       a[p] = a[p] + 1 if p in a else 1
-      n, p = n / p, 2
+      n, i, p = n / p, 0, ps[0]
   
   if p > 1:
     a[p] = a[p] + 1 if p in a else 1
   
   return prod([x + 1 for x in a.itervalues()])
-  
-d = n = 0
+
+ps = list(primes(10000000))
+
+d = n = 6
 
 while d < 500:
   n += 1
-  d = nd(n * (n + 1) / 2)
+  d = nd(n * (n + 1) / 2, ps)
 
 print n * (n + 1) / 2
 
 # answer: 76576500
-# runtime: 7.8s
+# runtime: 5.2s
